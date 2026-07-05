fe1c826f39b6dea53e4a43154588a5deb1c0188f
quantum <olcaymartin015@hotmail.com>
2026-07-06 00:09:29 +0300

    init

---
+++ ModmailApplication.java
@@ -0,0 +1,13 @@
+package com.quantum.modmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModmailApplication.class, args);
+	}
+
+}
