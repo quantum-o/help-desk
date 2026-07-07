58eaab7ff657dd5b1375fed0b297e394596918a1
quantum <olcaymartin015@hotmail.com>
2026-07-07 15:41:24 +0300

    feat(common): validate error response

--- common/exception/GlobalExceptionHandler.java
+++ common/exception/GlobalExceptionHandler.java
@@ -1,9 +1,15 @@
 package com.quantum.modmail.common.exception;
 
 import com.quantum.modmail.common.response.ApiErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,5 +18,18 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiErrorResponse> handleBusinessException(BusinessException ex) {
         ApiErrorResponse response = ApiErrorResponse.of(ex.getMessage(), ex.getErrorCode(), null);
         return ResponseEntity.status(ex.getStatus()).body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiErrorResponse.of("Validation failed", "VALIDATION_ERROR", errors));
     }
 }
