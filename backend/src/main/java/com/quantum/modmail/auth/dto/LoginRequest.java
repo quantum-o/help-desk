234721476aebd9e4162385236a7a44a926735f09
quantum <olcaymartin015@hotmail.com>
2026-07-07 19:44:14 +0300

    fix(LoginRequestDto): add all args constructor

--- backend/src/main/java/com/quantum/modmail/auth/dto/LoginRequest.java
+++ backend/src/main/java/com/quantum/modmail/auth/dto/LoginRequest.java
@@ -1,9 +1,11 @@
 package com.quantum.modmail.auth.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class LoginRequest {
     private String email;
 
