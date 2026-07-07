a667a590240a063a9897232a895e140e6078727d
quantum <olcaymartin015@hotmail.com>
2026-07-07 16:15:43 +0300

    chore!: changed root directory & file paths

---
+++ backend/src/test/java/com/quantum/modmail/ModmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantum.modmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
