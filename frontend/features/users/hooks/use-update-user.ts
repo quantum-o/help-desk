3c7707907d38b4795884a15d1441c189c2c97d5c
Olcay <olcaymartin015@hotmail.com>
2026-08-19 15:03:48 +0300

    fix(updateuser): invalidatequery instead of setquerydata

--- frontend/features/users/hooks/use-update-user.ts
+++ frontend/features/users/hooks/use-update-user.ts
@@ -1,19 +1,12 @@
 import { useMutation } from "@tanstack/react-query";
 import { queryClient } from "@/app/providers";
-import { ApiResponse } from "@/types/ApiResponse";
 import { updateUser } from "../api/update-user";
-import { User } from "@/types/User";
 
 export default function useUpdateUser() {
     return useMutation({
         mutationFn: updateUser,
-        onSuccess: (response: ApiResponse<User>) => {
-            queryClient.setQueryData(
-                ["users", response.data.id],
-                (oldData: User) => {
-                    if (!oldData) return oldData;
-                }
-            );
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["users"] });
         }
     })
 }
